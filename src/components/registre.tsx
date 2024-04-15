4b6255b4805ba0ebbf83af012d0c49b56a0f4c95
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-04-16 00:29:16 +0100

    add logo

--- src/components/registre.tsx
+++ src/components/registre.tsx
@@ -44,8 +44,8 @@ function Registre({setIsLogin}: Props) {
         ):(
             <div className='bg-secondary-content w-[60%] max-w-[70%] h-[70%] flex m-auto mt-10 rounded-lg max-md:bg-black max-lg:w-[80%] overflow-hidden'>
             <div className='w-[60%] flex flex-col max-md:w-[100%]'>
-                <h1 className='text-3xl text-center mt-3 text-secondary font-bold'>Sign Up</h1>
-                <form onSubmit={signUpHandle} className='h-[100%] flex flex-col w-[90%] m-auto justify-center gap-y-2 -mt-12'>
+                <h1 className='text-3xl text-center my-4 text-secondary font-bold'>Sign Up</h1>
+                <form onSubmit={signUpHandle} className='h-[90%] flex flex-col w-[90%] m-auto justify-center gap-y-2 -mt-12'>
                     <label htmlFor='email' className='font-bold text-secondary'>User name</label>
                     <input type="text" id='email' className='h-10 rounded-md' value={username} onChange={e=>setUsername(e.target.value)}/>
                     <label htmlFor='email' className='font-bold text-secondary'>Email</label>
@@ -56,6 +56,10 @@ function Registre({setIsLogin}: Props) {
                     <input type="password" id='passwordConf' className='h-10 rounded-md' value={passwordConf} onChange={e=>setPasswordConf(e.target.value)}/>
                     <button className='text-secondary border border-secondary py-2 w-[50%] mx-auto mt-5'>Create</button>
                 </form>
+                <div className='flex flex-row gap-x-12 m-auto relative bottom-8'>
+                    <Image alt='' src="/facbook.svg" height={38} width={38} />
+                    <Image alt='' src="/googl.svg" height={38} width={38} />
+                </div>
             </div>
             <div className='bg-secondary w-[40%] max-md:hidden raidus1 flex flex-col gap-y-5 justify-center items-center text-secondary-content'>
                 <Image alt='' className='text-' src="/fichiers4u.svg" height={60} width={60} />
@@ -63,6 +67,7 @@ function Registre({setIsLogin}: Props) {
                 <p className='text-xl text-center px-1'>Enter your personal details to use all of site features</p>
                 <button onClick={toggleAuthMode} className='border border-neutral-content px-7 py-2 rounded-md hover:bg-neutral-content transition-all ease-linear delay-50'>Sign In</button>
             </div>
+            
         </div>
         )
     }
