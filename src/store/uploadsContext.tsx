117d66ac843c4473001b5d1d74e371f6b7c59ad7
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-04-21 12:10:51 +0100

    add upload context

---
+++ src/store/uploadsContext.tsx
@@ -0,0 +1,13 @@
+import React, { useContext, useEffect, useState, createContext } from 'react'
+
+
+type Props = {}
+
+
+function UploadsContext({}: Props) {
+  return (
+    <div>UploadsContext</div>
+  )
+}
+
+export default UploadsContext
